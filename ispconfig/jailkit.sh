49aefa7b0b2c7d4359f3aa4ce6b00b8b62f7d62c
gOOvER <goover0976@gmail.com>
2019-04-14 08:28:26 +0200

    Update jailkit.sh
    Updated Jailkit Version

--- ispconfig/jailkit.sh
+++ ispconfig/jailkit.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 cd /tmp
-wget http://olivier.sessink.nl/jailkit/jailkit-2.19.tar.gz
-tar xvfz jailkit-2.19.tar.gz
-cd jailkit-2.19
+wget http://olivier.sessink.nl/jailkit/jailkit-2.20.tar.gz
+tar xvfz jailkit-2.20.tar.gz
+cd jailkit-2.20
 echo 5 > debian/compat
 ./debian/rules binary
 cd ..
-dpkg -i jailkit_2.19-1_*.deb
-rm -rf jailkit-2.19*
+dpkg -i jailkit_2.20-1_*.deb
+rm -rf jailkit-2.20*
